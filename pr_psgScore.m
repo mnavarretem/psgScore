401706b7acd602e4820644b4f43c2dc9ef140f19
mnavarretem <mnavarretem@gmail.com>
2022-02-23 17:43:34 -0500

    Update code
    Update code and repair bugs

--- pr_psgScore.m
+++ pr_psgScore.m
@@ -1,19 +1,48 @@
 function pr_psgScore
+%  psgScore is a graphical user interface (GUI) developed in MATLAB for
+% scoring of human sleep.
+% 
+% psgScore is an extensive, flexible and open source sleep diagnostic 
+% solution that simplifies the setup, review, analysis and reporting of 
+% experimental sleep studies.
+% 
+% psgScore is Fieldtrip compatible and requires Fieldtrip toolbox for 
+% reading files
+% 
+% psgScore implements different documented algorithms for sleep features 
+% detection, it provides several tools for signal visualization and 
+% manipulation, among which are found:
+% 
+% Channel(s) selection.
+% Display of multiple signal at the same time.
+% Several zoom and grid options (time, amplitude, etc.)
+% Possibility to make time and amplitude measurements directly on the signals.
+% Slow wave, spindle, eye movements, arousal detection.
+% 
+% 
+%   Written by:
+%   Miguel G. Navarrete Mejia, PhD
+%   UNIVERSIDAD DE LOS ANDES
+%   Colombia, 2021
+%   mnavarretem@gmail.com
+%   $Version: 0.5$ || $Date: 2021/11/13 10:30$
+%   This file is part of the psgScore project.
 %
-% Copyright (C) <2018>  <Miguel Navarrete>
+%%     Copyright (C) 2020, Miguel Navarrete
+%
+%     This program is free software: you can redistribute it and/or modify
+%     it under the terms of the GNU General Public License as published by
+%     the Free Software Foundation, either version 3 of the License, or
+%     (at your option) any later version.
 % 
-% This program is free software: you can redistribute it and/or modify
-% it under the terms of the GNU General Public License as published by
-% the Free Software Foundation, either version 3 of the License, or
-% (at your option) any later version.
+%     This program is distributed in the hope that it will be useful,
+%     but WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%     GNU General Public License for more details.
 % 
-% This program is distributed in the hope that it will be useful,
-% but WITHOUT ANY WARRANTY; without even the implied warranty of
-% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-% GNU General Public License for more details.
-% 
-% You should have received a copy of the GNU General Public License
-% along with this program.  If not, see <https://www.gnu.org/licenses/>.
+%     You should have received a copy of the GNU General Public License
+%     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 
 %% Initialize
 clc;clear;close all; close hidden
